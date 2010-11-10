6f477742e75ca11da9ad00bed4bd4aa95ec42a37
Nils Hoffmann <Nils.Hoffmann@cebitec.uni-bielefeld.de>
2010-11-10 10:42:30 +0000

    Added new Domain Objects for Database backed ChromA project.

---
+++ ChromaProjectType/src/net/sf/maltcms/chromaui/project/spi/types/UserDatabase.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.sf.maltcms.chromaui.project.spi.types;
+
+import java.net.URI;
+import net.sf.maltcms.chromaui.project.api.types.DatabaseType;
+import net.sf.maltcms.chromaui.project.api.types.IDatabase;
+
+/**
+ *
+ * @author hoffmann
+ */
+public class UserDatabase implements IDatabase{
+
+    private URI location;
+
+    private String name;
+
+    private final DatabaseType type = DatabaseType.USER;
+
+    @Override
+    public URI getLocation() {
+       return this.location;
+    }
+
+    @Override
+    public void setLocation(URI u) {
+        this.location = u;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public DatabaseType getType() {
+        return this.type;
+    }
+
+    @Override
+    public void setType(DatabaseType type) {
+
+    }
+
+}
