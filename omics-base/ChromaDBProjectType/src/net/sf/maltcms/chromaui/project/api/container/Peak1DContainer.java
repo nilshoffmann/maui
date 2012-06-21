8f40177dbd9ca5235093a6f34f290d9287aeab41
Nils Hoffmann <Nils.Hoffmann@cebitec.uni-bielefeld.de>
2012-06-21 13:31:45 +0000

    Fixed issues with transient field persistence in DB4oCrudProvider and Db4oInMemoryCrudProvider

--- omics-base/ChromaDBProjectType/src/net/sf/maltcms/chromaui/project/api/container/Peak1DContainer.java
+++ omics-base/ChromaDBProjectType/src/net/sf/maltcms/chromaui/project/api/container/Peak1DContainer.java
@@ -6,6 +6,7 @@ package net.sf.maltcms.chromaui.project.api.container;
 
 import com.db4o.activation.ActivationPurpose;
 import com.db4o.config.annotations.Indexed;
+import java.awt.Color;
 import java.awt.Image;
 import net.sf.maltcms.chromaui.project.api.descriptors.IChromatogramDescriptor;
 import net.sf.maltcms.chromaui.project.api.descriptors.IPeakAnnotationDescriptor;
@@ -18,6 +19,7 @@ import org.openide.util.ImageUtilities;
 public class Peak1DContainer extends ADatabaseBackedContainer<IPeakAnnotationDescriptor>{
 
     public final String PROP_CHROMATOGRAM = "chromatogram";
+    public final String PROP_COLOR = "color";
     
     @Indexed
     private IChromatogramDescriptor chromatogram;
@@ -32,6 +34,20 @@ public class Peak1DContainer extends ADatabaseBackedContainer<IPeakAnnotationDes
         IChromatogramDescriptor oldDescr = this.chromatogram;
         this.chromatogram = chromatogram;
         firePropertyChange(PROP_CHROMATOGRAM,oldDescr,this.chromatogram);
+    }
+    
+    private Color color = new Color(255,255,255,0);
+    
+    public Color getColor() {
+        activate(ActivationPurpose.READ);
+        return color;
+    }
+    
+    public void setColor(Color color) {
+        activate(ActivationPurpose.WRITE);
+        Color old = this.color;
+        this.color = color;
+        firePropertyChange(PROP_COLOR, old, this.color);
     }
     
     @Override
