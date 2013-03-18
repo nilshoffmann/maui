58aa6171eeec26c1560d9543c1cc392b124e7b2f
Nils Hoffmann <Nils.Hoffmann@cebitec.uni-bielefeld.de>
2013-03-18 08:17:54 +0000

    Added test for pca topcomponent.

---
+++ omics-base/PCAViewer/test/unit/src/de/unibielefeld/cebitec/lstutz/pca/visual/StandardGUITest.java
@@ -0,0 +1,88 @@
+/* 
+ * Maui, Maltcms User Interface. 
+ * Copyright (C) 2008-2012, The authors of Maui. All rights reserved.
+ *
+ * Project website: http://maltcms.sf.net
+ *
+ * Maui may be used under the terms of either the
+ *
+ * GNU Lesser General Public License (LGPL)
+ * http://www.gnu.org/licenses/lgpl.html
+ *
+ * or the
+ *
+ * Eclipse Public License (EPL)
+ * http://www.eclipse.org/org/documents/epl-v10.php
+ *
+ * As a user/recipient of Maui, you may choose which license to receive the code 
+ * under. Certain files or entire directories may not be covered by this 
+ * dual license, but are subject to licenses compatible to both LGPL and EPL.
+ * License exceptions are explicitly declared in all relevant files or in a 
+ * LICENSE file in the relevant directories.
+ *
+ * Maui is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. Please consult the relevant license documentation
+ * for details.
+ */
+package de.unibielefeld.cebitec.lstutz.pca.visual;
+
+import de.unibielefeld.cebitec.lstutz.pca.data.CSVParser;
+import de.unibielefeld.cebitec.lstutz.pca.data.ParserUtilities;
+import de.unibielefeld.cebitec.lstutz.pca.data.XMLParser;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import net.sf.maltcms.chromaui.statistics.pcaViewer.PCAViewerTopComponent;
+import org.openide.util.Exceptions;
+
+/**
+ *
+ * @author hoffmann
+ */
+public class StandardGUITest {
+	
+	public StandardGUITest() {
+	}
+
+	
+	public static void main(String[] args) {
+		String name = "MeltDB 3D Viewer";
+        URL url = StandardGUITest.class.getResource("/de/unibielefeld/cebitec/lstutz/pca/dataset/meltdb.xml");
+////        URL url = getClass().getResource("/de/unibielefeld/cebitec/lstutz/pca/dataset/end_test.xml");
+////        if (args[0].equals("xml")) {
+//        //ImportUtilities.get_url_input_stream(url)
+        XMLParser parser;
+        try {
+            parser = new XMLParser(url.openStream());
+            StandardGUI gui = new StandardGUI(name, ParserUtilities.group_data(parser.parse_data()));
+            PCAViewerTopComponent pvtc = new PCAViewerTopComponent();
+            pvtc.setData(gui);
+            pvtc.open();
+            pvtc.requestActive();
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+		URL url2 = StandardGUITest.class.getResource("/de/unibielefeld/cebitec/lstutz/pca/dataset/assault.txt");
+		InputStream is = null;
+		try {
+			is = url2.openStream();
+			CSVParser csvParser = new CSVParser(is);
+			StandardGUI gui = new StandardGUI(name, ParserUtilities.group_data(csvParser.parse_data()));
+            PCAViewerTopComponent pvtc = new PCAViewerTopComponent();
+            pvtc.setData(gui);
+            pvtc.open();
+            pvtc.requestActive();
+		} catch (IOException ex) {
+			Exceptions.printStackTrace(ex);
+		} finally {
+			if(is!=null) {
+				try{
+					is.close();
+				}catch(IOException ex) {
+					Exceptions.printStackTrace(ex);
+				}
+			}
+		}
+	}
+}
