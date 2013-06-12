dd3281bb2800681d863dccbf6192fdc5c4cadcfd
Henning Kuich <henning.kuich@mdc-berlin.de>
2013-06-12 09:41:22 +0200

    Revert "- replicated cosineSimilarity comparison from the pipelineTestModule in this Maui module (just test code to make sure i can get to the right data...), using normal intensities and not binned, since one needs denseArrayProducer for that... Can now define time-windows inwhich to find spectra and do comparisons between them."
    This reverts commit f26254b1d1264bf4c0604a0f32f02425933d84b1.

---
+++ maui-ext/retentionIndexCalculation/src/de/mdcberlin/hkuich/retentionIndexCalculation/assistedRICalculation.java
@@ -0,0 +1,33 @@
+package de.mdcberlin.hkuich.retentionIndexCalculation;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import net.sf.maltcms.chromaui.project.api.IChromAUIProject;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle.Messages;
+
+
+@ActionReferences({
+    @ActionReference(path = "Actions/ChromAUIProjectLogicalView/DefaultActions")})
+@ActionID(
+        category = "Maui",
+        id = "de.mdcberlin.hkuich.retentionIndexCalculation.assistedRICalculation")
+@ActionRegistration(
+        displayName = "#CTL_assistedRICalculation")
+@Messages("CTL_assistedRICalculation=assistedRICalculation")
+public final class assistedRICalculation implements ActionListener {
+
+    private final IChromAUIProject context;
+
+    public assistedRICalculation(IChromAUIProject context) {
+        this.context = context;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ev) {
+        // TODO use context
+    }
+}
