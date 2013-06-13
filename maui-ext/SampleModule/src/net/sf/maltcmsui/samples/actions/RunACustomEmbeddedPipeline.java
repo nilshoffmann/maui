7459f967163b12778869350857529115ec50b62e
Nils Hoffmann <nilshoffmann@users.sf.net>
2013-06-13 15:41:46 +0200

    Updated sample action.

--- maui-ext/SampleModule/src/net/sf/maltcmsui/samples/actions/RunACustomEmbeddedPipeline.java
+++ maui-ext/SampleModule/src/net/sf/maltcmsui/samples/actions/RunACustomEmbeddedPipeline.java
@@ -28,29 +28,49 @@
 package net.sf.maltcmsui.samples.actions;
 
 import cross.commands.fragments.IFragmentCommand;
+import cross.datastructures.fragments.FileFragment;
 import cross.datastructures.fragments.IFileFragment;
 import cross.datastructures.pipeline.CommandPipeline;
 import cross.datastructures.tuple.TupleND;
 import cross.datastructures.workflow.DefaultWorkflow;
+import cross.datastructures.workflow.IWorkflowResult;
+import cross.event.IEvent;
+import cross.event.IListener;
 import cross.tools.StringTools;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import maltcms.commands.fragments.preprocessing.DenseArrayProducer;
+import maltcms.datastructures.ms.IChromatogram;
+import maltcms.datastructures.peak.Peak1D;
+import maltcms.datastructures.peak.PeakType;
+import maltcms.datastructures.peak.normalization.IPeakNormalizer;
 import net.sf.maltcms.chromaui.project.api.IChromAUIProject;
+import net.sf.maltcms.chromaui.project.api.container.Peak1DContainer;
 import net.sf.maltcms.chromaui.project.api.descriptors.IChromatogramDescriptor;
+import net.sf.maltcms.chromaui.project.api.descriptors.IPeakAnnotationDescriptor;
 import net.sf.maltcms.chromaui.ui.support.api.AProgressAwareRunnable;
+import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ProjectManager;
+import org.netbeans.api.project.ui.OpenProjects;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
+import org.openide.filesystems.FileUtil;
 import org.openide.util.Exceptions;
 import org.openide.util.NbBundle.Messages;
+import ucar.ma2.ArrayDouble;
 
 @ActionID(
 		category = "Maui",
@@ -102,28 +122,85 @@ public final class RunACustomEmbeddedPipeline implements ActionListener {
 				 * List of input file fragments
 				 */
 				List<IFileFragment> fragments = new ArrayList<IFileFragment>();
-				CommandPipeline cp = new CommandPipeline();
-				for (IChromatogramDescriptor chromatogram : project.getChromatograms()) {
-					fragments.add(chromatogram.getChromatogram().getParent());
+				File outputDir = project.getOutputLocation(RunACustomEmbeddedPipeline.class);
+				
+				/*
+				 * Write current peak lists as Peak1D output for Maltcms access
+				 */
+				File peakListTmpDirectory = new File(outputDir,"peakListTmpDir");
+				peakListTmpDirectory.mkdirs();
+				for (IChromatogramDescriptor chromatogramDescr : project.getChromatograms()) {
+					IChromatogram chrom = chromatogramDescr.getChromatogram();
+					List<Peak1D> peaksForChromatogram = new ArrayList<Peak1D>();
+					for(Peak1DContainer peakContainer:project.getPeaks(chromatogramDescr)){
+						for(IPeakAnnotationDescriptor pad:peakContainer.getMembers()) {
+							Peak1D p = new Peak1D(chrom.getIndexFor(pad.getStartTime()), chrom.getIndexFor(pad.getApexTime()), chrom.getIndexFor(pad.getStopTime()));
+							p.setStartTime(pad.getStartTime());
+							p.setApexTime(pad.getApexTime());
+							p.setStopTime(pad.getStopTime());
+							p.setFile(chrom.getParent().getName());
+							p.setMw(pad.getUniqueMass());
+							//could also be EIC_RAW, EIC_FILTERED, TIC_RAW, TIC_FILTERED
+							p.setPeakType(PeakType.UNDEFINED);
+							p.setSnr(pad.getSnr());
+							p.setArea(pad.getArea());
+							p.setNormalizationMethods(pad.getNormalizationMethods());
+							p.setNormalizedArea(pad.getNormalizedArea());
+							p.setBaselineStartTime(pad.getStartTime());
+							p.setBaselineStopTime(pad.getStopTime());
+							peaksForChromatogram.add(p);
+						}
+					}
+					//sort by apex retention time, ascending
+					Collections.sort(peaksForChromatogram,new Comparator<Peak1D>() {
+
+						@Override
+						public int compare(Peak1D o1, Peak1D o2) {
+							return Double.compare(o1.getApexTime(), o2.getApexTime());
+						}
+					});
+					FileFragment peakFile = new FileFragment(new File(peakListTmpDirectory,chrom.getParent().getName()));
+					peakFile.addSourceFile(chrom.getParent());
+					Peak1D.append(peakFile, new LinkedList<IPeakNormalizer>(), peaksForChromatogram, chrom.getParent().getChild("total_intensity").getArray(), "tic_peaks", "tic_filtered");
+					peakFile.save();
+					//avoid memory leaks
+					fragments.add(new FileFragment(peakFile.getUri()));
 				}
+				/*
+				 * Prepare the command pipeline
+				 */
+				CommandPipeline cp = new CommandPipeline();
 				cp.setCommands(commands);
 				cp.setInput(new TupleND<IFileFragment>(fragments));
 				DefaultWorkflow dw = new DefaultWorkflow();
 				dw.setStartupDate(new Date());
-				dw.setName("henningsWorkflow");
 				dw.setCommandSequence(cp);
 				//false does not work at the moment
 				dw.setExecuteLocal(true);
-				dw.setOutputDirectory(project.getOutputLocation(RunACustomEmbeddedPipeline.class));
+				dw.setOutputDirectory(outputDir);
+				dw.addListener(new IListener<IEvent<IWorkflowResult>>() {
+
+					@Override
+					public void listen(IEvent<IWorkflowResult> v) {
+						Logger.getLogger(RunACustomEmbeddedPipeline.class.getName()).log(Level.INFO, "Received workflow result {0}", v.get());
+					}
+				});
 				try {
 					TupleND<IFileFragment> results = dw.call();
 					//map output results to input, they may be in the same order as the original input
 					Map<IChromatogramDescriptor,IFileFragment> inputToOutputMap = new LinkedHashMap<IChromatogramDescriptor,IFileFragment>();
+					Logger.getLogger(RunACustomEmbeddedPipeline.class.getName()).log(Level.INFO, "Received {0} result files!", results.size());
 					for (IChromatogramDescriptor chromatogram : project.getChromatograms()) {
-						String bareChromName = StringTools.removeFileExt(chromatogram.getName());
+						String chromName = chromatogram.getName();
+						if("<NA>".equals(chromName)) {
+							chromName = StringTools.removeFileExt(chromatogram.getChromatogram().getParent().getName());
+						}
+						Logger.getLogger(RunACustomEmbeddedPipeline.class.getName()).log(Level.INFO, "Mapping chromatogram {0} to ", chromName);
 						for(IFileFragment fragment:results) {
 							String bareFragmentName = StringTools.removeFileExt(fragment.getName());
-							if(bareChromName.equals(bareFragmentName)) {
+							Logger.getLogger(RunACustomEmbeddedPipeline.class.getName()).log(Level.INFO, "{0}", bareFragmentName);
+							if(chromName.equals(bareFragmentName)) {
+								Logger.getLogger(RunACustomEmbeddedPipeline.class.getName()).info("Match!");
 								inputToOutputMap.put(chromatogram, fragment);
 							}
 						}
@@ -131,7 +208,13 @@ public final class RunACustomEmbeddedPipeline implements ActionListener {
 					for(IChromatogramDescriptor chromatogram: inputToOutputMap.keySet()) {
 						//retrieve result file
 						IFileFragment resultFile = inputToOutputMap.get(chromatogram);
+						Logger.getLogger(RunACustomEmbeddedPipeline.class.getName()).info(resultFile.toString());
 					}
+					/*
+					 * Open the maltcms workflow as a project in the UI
+					 */
+					Project maltcmsWorkflowProject = ProjectManager.getDefault().findProject(FileUtil.toFileObject(dw.getOutputDirectory()));
+					OpenProjects.getDefault().open(new Project[]{maltcmsWorkflowProject}, false);
 				} catch (Exception ex) {
 					Exceptions.printStackTrace(ex);
 				}
