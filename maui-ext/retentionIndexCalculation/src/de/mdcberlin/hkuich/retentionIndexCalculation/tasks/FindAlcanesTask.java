238a3b187fb0db546e06e221f8a984e3254c8a73
Henning Kuich <hkuich@gmail.com>
2013-06-25 17:03:41 +0200

    New commit: PHJLK-RIC-005 - Commented out a bunch of the previous playing around code but left it in for commit. Created a new function called findNGlobalMaxima, which takes a peaklist, a double array of masses that are searched for their max, and an int that defines how many maxima should be found per mass lane. The function create a 2D int array, rows = number of mass lanes passed in, columns = number of peaks in the peak list. Then we populate this 2D array with the intensity values of the desired mass lanes. The row is the mass lane, the column now the intensity value of all peaks of that mass lane in sequence. The peak counter lists the number of peaks. I then define a maxima array, where we will put the 2bynumberOfMaxima array that will contain the maxima per massLane. I also define the final List of 2D arrays that will hold all arrays of maxima of massLanes. We set the manima array to zero at all values and check if the current intensity of the currentMass Lane is higher than the last intensity in the maxima array, if so, add the index (peakNumber) and the associated intensity to the maxima array and sort the array so that lowest intensity is at the bottom of the array, where the next value will be compared to. Do this for all peaks within one massLane, then finally sort the maxima array and add it to the resultList, and start with the next massLane and repeat. Then return the resultList, an ArrayList of 2D int arrays.

--- maui-ext/retentionIndexCalculation/src/de/mdcberlin/hkuich/retentionIndexCalculation/tasks/FindAlcanesTask.java
+++ maui-ext/retentionIndexCalculation/src/de/mdcberlin/hkuich/retentionIndexCalculation/tasks/FindAlcanesTask.java
@@ -4,6 +4,7 @@ import cross.datastructures.tuple.Tuple2D;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.TreeMap;
 import lombok.Data;
@@ -44,84 +45,196 @@ public class FindAlcanesTask extends AProgressAwareRunnable implements Serializa
                     getProgressHandle().progress("Annotating peaks on " + chrom.getDisplayName(), cnt);
 
                     for (Peak1DContainer container : context.getPeaks(chrom)) {
-                            if(isCancel()) {
-                                    return;
-                            }
-                            getProgressHandle().progress("Annotating peaks on container " + container.getDisplayName());
+                        if(isCancel()) {
+                                return;
+                        }
+                        getProgressHandle().progress("Annotating peaks on container " + container.getDisplayName());
+
+                        System.out.println("Resetting peak match information!");
+                        for (IPeakAnnotationDescriptor ipad : container.getMembers()) {
+                                if(isCancel()) {
+                                        return;
+                                }
+                                ipad.setSimilarity(Double.NaN);
+                                ipad.setNativeDatabaseId("NA");
+                                ipad.setName("Unknown Compound");
+                                ipad.setFormula("NA");
+                                ipad.setDisplayName("Unknown Compound");
+                                ipad.setLibrary("custom");
+                                ipad.setRetentionIndex(Double.NaN);
+                        }
+
+                        //container.getMembers() contains all the peaks. 
+                        ArrayList<IPeakAnnotationDescriptor> peaklist = new ArrayList<IPeakAnnotationDescriptor>(container.getMembers());
+                        System.out.println("Finding Alcanes in container: " + container.getDisplayName());
+                        int peakIndex = 0;
+
+                        for (IPeakAnnotationDescriptor peak : peaklist) { 
+                            IScan scan = new Scan1D(Array.factory(peak.getMassValues()), Array.factory(peak.getIntensityValues()), peak.getIndex(), peak.getApexTime());
+
+//                              AMetabolitePredicate amp = predicate.copy();
+//				amp.setMaxHits(5);
+//                              amp.setScoreThreshold(0.9);         
+//				amp.setScan(scan);
+//                              AMetabolitePredicate queryPredicate = amp;                                
+
+                            double similarity = cosineSimilarity(Array.factory(peak.getIntensityValues()),Array.factory(peak.getIntensityValues()));
+                            /*System.out.println("Array.factory(peak.getMassValues()): " + Array.factory(peak.getMassValues()));
+                            System.out.println("Array.factory(peak.getIntensityValues()): " + Array.factory(peak.getIntensityValues()));
+                            System.out.println("CosineSimilarity of identical peaks: " + similarity);
+                            System.out.println("peak.getIndex(): " + peak.getIndex()); 
+                            System.out.println("peakIndex: " + peakIndex); */
+                            peakIndex++;
+                        }
                             
-                            System.out.println("Resetting peak match information!");
-                            for (IPeakAnnotationDescriptor ipad : container.getMembers()) {
-                                    if(isCancel()) {
-                                            return;
-                                    }
-                                    ipad.setSimilarity(Double.NaN);
-                                    ipad.setNativeDatabaseId("NA");
-                                    ipad.setName("Unknown Compound");
-                                    ipad.setFormula("NA");
-                                    ipad.setDisplayName("Unknown Compound");
-                                    ipad.setLibrary("custom");
-                                    ipad.setRetentionIndex(Double.NaN);
-                            }
+                        double[] alcaneMasses = {71.0,85.0,99.0};
+                        int numberOfMaxima = 9;
+
+                        findNGlobalMaxima(peaklist, alcaneMasses, numberOfMaxima);
                             
-                            //container.getMembers() contains all the peaks. 
-                            ArrayList<IPeakAnnotationDescriptor> peaklist = new ArrayList<IPeakAnnotationDescriptor>(container.getMembers());
-                            System.out.println("Finding Alcanes in container: " + container.getDisplayName());
-                            
-                            for (IPeakAnnotationDescriptor peak : peaklist) {
-                                AMetabolitePredicate amp = predicate.copy();
-				amp.setMaxHits(5);
-                                amp.setScoreThreshold(0.9);
-                                IScan scan = new Scan1D(Array.factory(peak.getMassValues()), Array.factory(peak.getIntensityValues()), peak.getIndex(), peak.getApexTime());
-                                System.out.println("Array.factory(peak.getMassValues()): " + Array.factory(peak.getMassValues()));
-				amp.setScan(scan);
-                                AMetabolitePredicate queryPredicate = amp;                                
-                                
-                                double similarity = cosineSimilarity(Array.factory(peak.getIntensityValues()),Array.factory(peak.getIntensityValues()));
-                                System.out.println("Array.factory(peak.getIntensityValues()): " + Array.factory(peak.getIntensityValues()));
-                                System.out.println("CosineSimilarity of identical peaks: " + similarity);
-                                
-                            }
-                            
-//                            IQuery<IPeakAnnotationDescriptor> query = Lookup.getDefault().lookup(
-//                                            IQueryFactory.class).createQuery(databases, ricalc, predicate, matchThreshold, maxNumberOfHits,
-//                                            new ArrayList<IPeakAnnotationDescriptor>(container.getMembers()), riWindow);
-//                            System.out.println("Created query for container: " + container.getDisplayName());
-//                            try {
-//                                    System.out.println("Running query on container: " + container.getDisplayName());
-//                                    List<QueryResultList<IPeakAnnotationDescriptor>> results = query.call();
-//
-//                                    for (QueryResultList<IPeakAnnotationDescriptor> resultList : results) {
-//                                            if(isCancel()) {
-//                                                    return;
-//                                            }
-//                                            for (final IQueryResult<IPeakAnnotationDescriptor> result : resultList) {
-//                                                    if(isCancel()) {
-//                                                            return;
-//                                                    }
-//                                                    IChromatogramDescriptor icd = result.getScan().getChromatogramDescriptor();
-//                                                    IPeakAnnotationDescriptor ipad = result.getScan();
-//                                                    if (result.getMetabolites().size() > 0) {
-//                                                            System.out.println("Found " + result.getMetabolites().size() + " matches above threshold!");
-//                                                            IMetabolite bestHit = result.getMetabolites().get(0);
-//                                                            ipad.setNativeDatabaseId(bestHit.getID());
-//                                                            ipad.setSimilarity(result.getScoreFor(bestHit));
-//                                                            ipad.setName(bestHit.getName());
-//                                                            ipad.setFormula(bestHit.getFormula());
-//                                                            ipad.setDisplayName(bestHit.getName());
-//                                                            ipad.setLibrary(result.getDatabaseDescriptor().getName());
-//                                                            ipad.setRetentionIndex(result.getRetentionIndex());
-//                                                    }
-//                                            }
-//                                    }
-//                            } catch (Exception ex) {
-//                                    Exceptions.printStackTrace(ex);
-//                            }
                     }
                     cnt++;
             }
         } finally {
                 getProgressHandle().finish();
         }
+    }
+    
+    private ArrayList<Integer[][]> findNGlobalMaxima(final ArrayList<IPeakAnnotationDescriptor> peaklist, double[] NMasses, int numberOfMaxima) {
+        
+        System.out.println("Entered Function");
+        for(int f = 0; f<NMasses.length;f++){
+            System.out.println("NMasses: " + NMasses[f]);
+        }
+        
+        //put the massLanes into an int array, each row being one mass lane, each column being one peak, the values being ints of intensity
+        int[][] NMassMatrix = new int[NMasses.length][peaklist.size()];        
+        int peakCounter = 0;
+        for (IPeakAnnotationDescriptor peak : peaklist) { 
+            IScan scan = new Scan1D(Array.factory(peak.getMassValues()), Array.factory(peak.getIntensityValues()), peak.getIndex(), peak.getApexTime());
+            
+            for(int massLane = 0; massLane<NMasses.length;massLane++){
+                int[] currentPeakIntensities = peak.getIntensityValues();
+                double[] currentMasses = peak.getMassValues();
+                int massLaneIndex = 0;
+                
+                //get the correct index for the NMasses
+                for(int i = 0; i<currentMasses.length;i++)
+                {
+                    if(NMasses[massLane] == currentMasses[i])
+                    {
+                        massLaneIndex = i;
+                    }
+                }
+                int currentMassLaneIntensity = currentPeakIntensities[massLaneIndex];
+                NMassMatrix[massLane][peakCounter] = currentMassLaneIntensity;
+            }                        
+            peakCounter++;
+        }
+        
+        //QualityControl
+        for(int i = 0; i < NMasses.length; i++)
+        {
+            for(int j = 0; j<peaklist.size();j++)
+            {
+                System.out.print(NMassMatrix[i][j] + "\t");
+            }
+            System.out.println();
+        }       
+        
+        //now iterate over the three arrays an keep the indeces of the nine highest numbers, then sort them ascendingly, then print them
+        Integer maxima[][] = new Integer[numberOfMaxima][2];
+        ArrayList<Integer[][]> resultList = new ArrayList<Integer[][]>();
+        
+        
+        
+//        for(int x = 0; x < numberOfMaxima; x++)
+//        {
+//            for(int y = 0; y<2;y++)
+//            {
+//                System.out.print(maxima[x][y] + "\t");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println("to here?");
+        
+        for(int i = 0; i < NMasses.length; i++)
+        {
+            //set array to 0 in all cases
+            for(int x = 0; x < numberOfMaxima; x++)
+            {
+                for(int y = 0; y<2;y++)
+                {
+                    maxima[x][y] = 0;
+                }
+            }
+            
+            //now populate maxima array for current Mass lane (given by i)
+            for(int j = 0; j<peaklist.size();j++)
+            {
+                if(NMassMatrix[i][j] > maxima[numberOfMaxima-1][1])
+                {
+                    //save index
+                    maxima[numberOfMaxima-1][0] = j;
+                    //save actual value
+                    maxima[numberOfMaxima-1][1] = NMassMatrix[i][j];
+                    
+//                    for(int x = 0; x < numberOfMaxima; x++)
+//                    {
+//                        for(int y = 0; y<2;y++)
+//                        {
+//                            System.out.print(maxima[x][y] + "\t");
+//                        }
+//                        System.out.println();
+//                    }
+                    
+                    Arrays.sort(maxima, new Comparator<Integer[]>()
+                    {
+                        @Override
+                        public int compare(Integer[] int1, Integer[] int2)
+                        {
+                            Integer numOfKeys1 = int1[1];
+                            Integer numOfKeys2 = int2[1];
+                            return numOfKeys2.compareTo(numOfKeys1);
+                        }
+                    });
+                    
+//                    for(int x = 0; x < numberOfMaxima; x++)
+//                    {
+//                        for(int y = 0; y<2;y++)
+//                        {
+//                            System.out.print(maxima[x][y] + "\t");
+//                        }
+//                        System.out.println();
+//                    }
+                }
+            }
+            Arrays.sort(maxima, new Comparator<Integer[]>()
+            {
+                @Override
+                public int compare(Integer[] int1, Integer[] int2)
+                {
+                    Integer numOfKeys1 = int1[0];
+                    Integer numOfKeys2 = int2[0];
+                    return numOfKeys1.compareTo(numOfKeys2);
+                }
+            });                    
+
+            System.out.println("maximaArray for Number: " + i + " and massLame: " + NMasses[i]);
+            for(int x = 0; x < numberOfMaxima; x++)
+            {
+                for(int y = 0; y<2;y++)
+                {
+                    System.out.print(maxima[x][y] + "\t");
+                }
+                System.out.println();
+            }
+            System.out.println();
+            System.out.println();
+            resultList.add(maxima);
+        }
+       
+        return resultList;
     }
     
     private double cosineSimilarity(final Array t1, final Array t2) {
