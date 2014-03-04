71284e650aa76f994f23e92b30fe2190b91ca26d
Nils Hoffmann <nilshoffmann@users.sf.net>
2014-03-04 08:11:51 +0100

    Updated cross and maltcms versions to 1.3.1.

--- maui/ChartComponents/src/net/sf/maltcms/chromaui/charts/events/Chromatogram2DMSProvider.java
+++ maui/ChartComponents/src/net/sf/maltcms/chromaui/charts/events/Chromatogram2DMSProvider.java
@@ -1,5 +1,5 @@
-/* 
- * Maui, Maltcms User Interface. 
+/*
+ * Maui, Maltcms User Interface.
  * Copyright (C) 2008-2012, The authors of Maui. All rights reserved.
  *
  * Project website: http://maltcms.sf.net
@@ -14,10 +14,10 @@
  * Eclipse Public License (EPL)
  * http://www.eclipse.org/org/documents/epl-v10.php
  *
- * As a user/recipient of Maui, you may choose which license to receive the code 
- * under. Certain files or entire directories may not be covered by this 
+ * As a user/recipient of Maui, you may choose which license to receive the code
+ * under. Certain files or entire directories may not be covered by this
  * dual license, but are subject to licenses compatible to both LGPL and EPL.
- * License exceptions are explicitly declared in all relevant files or in a 
+ * License exceptions are explicitly declared in all relevant files or in a
  * LICENSE file in the relevant directories.
  *
  * Maui is distributed in the hope that it will be useful, but WITHOUT
@@ -28,14 +28,13 @@
 package net.sf.maltcms.chromaui.charts.events;
 
 import cross.datastructures.fragments.IFileFragment;
-import ucar.ma2.Array;
 import cross.datastructures.tuple.Tuple2D;
 import java.awt.Point;
 import maltcms.datastructures.caches.IScanLine;
 import maltcms.datastructures.caches.ScanLineCacheFactory;
-import maltcms.datastructures.ms.ChromatogramFactory;
 import maltcms.datastructures.ms.IChromatogram2D;
 import maltcms.datastructures.ms.IScan2D;
+import ucar.ma2.Array;
 
 public class Chromatogram2DMSProvider implements MassSpectrumProvider<IScan2D> {
 
@@ -46,50 +45,50 @@ public class Chromatogram2DMSProvider implements MassSpectrumProvider<IScan2D> {
     private final IChromatogram2D chrom;
 
     public Chromatogram2DMSProvider(IChromatogram2D f, int sl, int spm) {
-            this.sl = sl;
-            this.spm = spm;
-            this.iff = f.getParent();
-            this.chrom = f;
-            System.out.println("Initing scanlinecache");
-            this.isl = ScanLineCacheFactory.getScanLineCache(this.iff);
-            System.out.println("done initing scanlinecache");
+        this.sl = sl;
+        this.spm = spm;
+        this.iff = f.getParent();
+        this.chrom = f;
+        System.out.println("Initing scanlinecache");
+        this.isl = ScanLineCacheFactory.getScanLineCache(this.iff);
+        System.out.println("done initing scanlinecache");
     }
-	
-	/* (non-Javadoc)
+
+    /* (non-Javadoc)
      * @see maltcms.ui.events.MSChartHandler.MassSpectrumProvider#getMS(int)
      */
     @Override
     public Tuple2D<Array, Array> getMS(int index) {
-        return this.isl.getSparseMassSpectra(index/sl, index%sl);
-        //return MaltcmsTools.getMS(this.iff,index);
-    }
-    
-    public Tuple2D<Array,Array> getMS(int x, int y) {
-        int index = (x*spm) + y;
-    	return this.isl.getSparseMassSpectra(x, y);
+        return this.isl.getSparseMassSpectrum(index / sl, index % sl);
         //return MaltcmsTools.getMS(this.iff,index);
     }
 
-	/* (non-Javadoc)
+    public Tuple2D<Array, Array> getMS(int x, int y) {
+        int index = (x * spm) + y;
+        return this.isl.getSparseMassSpectrum(x, y);
+        //return MaltcmsTools.getMS(this.iff,index);
+    }
+
+    /* (non-Javadoc)
      * @see maltcms.ui.events.MassSpectrumProvider#getIndex(double)
      */
     @Override
     public int getIndex(double rt) {
-	    return this.chrom.getIndexFor(rt);
+        return this.chrom.getIndexFor(rt);
     }
 
-	/* (non-Javadoc)
+    /* (non-Javadoc)
      * @see maltcms.ui.events.MassSpectrumProvider#getRT(int)
      */
     @Override
     public double getRT(int index) {
-    	return this.chrom.getScanAcquisitionTime().getDouble(index);
+        return this.chrom.getScanAcquisitionTime().getDouble(index);
     }
 
     @Override
     public IScan2D getScan(int index) {
         Point p = this.chrom.getPointFor(index);
-        return this.chrom.getScan2D(p.x,p.y);
+        return this.chrom.getScan2D(p.x, p.y);
     }
-	
+
 }
